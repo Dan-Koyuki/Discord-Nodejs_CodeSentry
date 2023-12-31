ff665969dfd0f9f945ba07752a205b62c1bf7969
Dan-Koyuki <dankoyuki398@gmail.com>
2023-12-31 10:42:12 +0700

    test bot vercel

--- src/bot.js
+++ src/bot.js
@@ -29,7 +29,7 @@ for (const folder of functionsFolder) {
     .filter((file) => file.endsWith(".js"));
 
   for (const file of functionsFiles)
-    require(`./src/functions/${folder}/${file}`)(client);
+    require(path.join(__dirname, "functions", folder, file))(client);
 }
 
 client.handleEvents();
