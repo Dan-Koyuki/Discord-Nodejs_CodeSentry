1e9cad08a24ab16f13a68e19076560ece4ec6b0c
Dan-Koyuki <dankoyuki398@gmail.com>
2024-02-17 07:02:52 +0700

    embed test

---
+++ src/commands/slashCommands/utility/embed.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("Return My Embed Example!"),
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+      .setTitle("My Embed")
+      .setDescription("Embed Example")
+      .setColor('Navy')
+      .setImage(client.user.displayAvatarURL())
+      .setThumbnail('https://res.cloudinary.com/dankoyuki/image/upload/v1706862368/Custom%20Card/l3dulnz3rs8j8gnaydnj.png')
+      .setTimestamp(Date.now())
+      .setAuthor({
+        url: 'https://dan-koyuki-profile.vercel.app',
+        iconURL: interaction.user.displayAvatarURL(),
+        name: interaction.user.tag
+      })
+      .setFooter({
+        iconURL: client.user.displayAvatarURL(),
+        text: client.user.tag
+      })
+      .addFields([
+        {
+          name: 'Field 1',
+          value: 'Value of Field 1',
+          inline: true
+        },
+        {
+          name: 'Field 2',
+          value: 'Value of Field 2',
+          inline: true
+        },
+      ])
+  },
+};
