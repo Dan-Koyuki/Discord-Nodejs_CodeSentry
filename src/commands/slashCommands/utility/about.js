4b3d34699b8a78f272b8e58d0bb1ca9d61329a0a
Dan-Koyuki <dankoyuki398@gmail.com>
2024-02-17 07:43:53 +0700

    more embed test

--- src/commands/slashCommands/utility/about.js
+++ src/commands/slashCommands/utility/about.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName(`Dan Koyuki`)
+    .setName(`Dan\ Koyuki`)
     .setDescription("Author Contact!"),
   async execute(interaction, client) {
     const embed = new EmbedBuilder()
