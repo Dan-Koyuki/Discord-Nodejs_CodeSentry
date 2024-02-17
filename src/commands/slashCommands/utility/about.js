5c49cd7fa11776a92cc7dd2b4b59e5d7a5fc6113
Dan-Koyuki <dankoyuki398@gmail.com>
2024-02-17 07:34:28 +0700

    more embed test

---
+++ src/commands/slashCommands/utility/about.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("Dan Koyuki")
+    .setDescription("Author Contact!"),
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+      .setTitle(`Hello, ${interaction.user.tag}`)
+      .setDescription(`Currently Nothing!`)
+      .setColor('Navy')
+      .setThumbnail(client.user.displayAvatarURL())
+      .setTimestamp(Date.now())
+      .setAuthor({
+        url: 'https://dan-koyuki-profile.vercel.app',
+        iconURL: 'https://res.cloudinary.com/dankoyuki/image/upload/v1706862368/Custom%20Card/l3dulnz3rs8j8gnaydnj.png',
+        name: 'Dan Koyuki'
+      })
+      .setFooter({
+        iconURL: client.user.displayAvatarURL(),
+        text: client.user.tag
+      });
+
+      await interaction.reply({
+        embeds: [embed]
+      });
+  },
+};
